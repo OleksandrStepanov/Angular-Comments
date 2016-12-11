22be23aed6e411abf88c448d6b8bb352507e9a9c
OleksandrStepanov <cbrzelviv@ukr.net>
2016-12-11 23:33:11 +0200

    add angular-comments

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var app = angular.module("app", []);
+
+app.controller("AddThemeForCommentCtrl", function AddThemeForCommentCtrl ($scope) {
+
+    $scope.saved = localStorage.getItem('themes');
+    $scope.themes = (localStorage.getItem('themes')!=="") ? JSON.parse($scope.saved) : [];
+    localStorage.setItem('themes', JSON.stringify($scope.themes));
+
+    $scope.addTheme = function() {
+        $scope.themes.push({
+            text: $scope.todoText,
+            comment: []
+        });
+        $scope.todoText = ''; //clear the input after adding
+        localStorage.setItem('themes', JSON.stringify($scope.themes));
+    };
+
+    $scope.addComment = function() {
+        $scope.themes[$scope.selectedIndex].comment.push($scope.newComment);
+        $scope.newComment = ''; //clear the input after adding
+        localStorage.setItem('themes', JSON.stringify($scope.themes));
+    };
+
+    $scope.selectedIndex = 0;
+    $scope.select= function(i) {
+        $scope.selectedIndex=i;
+    };
+
+    $scope.delTheme = function (index) {
+        $scope.themes.splice(index, 1);
+        localStorage.removeItem(index);
+        localStorage.setItem('themes', JSON.stringify($scope.themes));
+    };
+});
+
